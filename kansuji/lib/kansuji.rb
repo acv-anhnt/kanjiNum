347372e8536ff45ae6e6967db2e4b5379b668a4e
acv-anhnt <anhnt@atmrkcafe.org>
2018-11-14 18:17:08 +0700

    Modify the name of method and implement to_kanji method

--- kansuji/lib/kansuji.rb
+++ kansuji/lib/kansuji.rb
@@ -3,41 +3,74 @@ class Error < StandardError; end
 Kanji = Hash["京" => 10**16, "兆" => 10**12, "億" => 10**8, "万" => 10**4, "千" => 1000, "百" => 100, "十" => 10,
              "九" => 9, "八" => 8, "七" => 7, "六" => 6, "五" => 5, "四" => 4, "三" => 3, "二" => 2, "一" => 1, 
              "零" => 0, "〇" => 0]
-Break = Array[10**16, 10**12, 10**8, 10**4, 1000, 100, 10]
-
+Break = Array[10**16, 10**12, 10**8, 10000 , 1000, 100, 10]
 class KansujiHelp
   def segmentVal (array)
-      return 1 if array.length == 0 #Case: [100, 10]
-      array.each_with_index do |a, i|
-        if (i < array.length-1 and a < array[i+1]) then
-          array[i] = 0
-          array[i+1] = a*array[i+1]
-        end
+    return 1 if array.length == 0 #Case: [100, 10]
+    array.each_with_index do |a, i|
+      if (i < array.length-1 and a < array[i+1]) then
+        array[i] = 0
+        array[i+1] = a*array[i+1]
       end
-      return array.reduce(:+)
     end
+    return array.reduce(:+)
+  end
+  def onePartToKanji (partValue)
+    kanjiString = ""
+    Break.each do |b|
+      intPart = partValue/b
+      if intPart == 1 then
+        kanjiString += Kanji.key(b)
+        partValue -= b
+      end
+      if intPart > 1 then
+        kanjiString += Kanji.key(intPart) + Kanji.key(b)
+        partValue -= intPart*b
+      end
+    end
+    restPart = partValue != 0 ? Kanji.key(partValue): ""
+    return kanjiString + restPart
+  end
 end
-
 class String
-    def to_number
-      kansujiHelp = KansujiHelp.new()
-      numberValueArray = Array.new()
-      sum = 0
-      self.split('').each do |c| numberValueArray.push(Kanji[c]) end  #Convert the Kanji String to number value array
-      return numberValueArray[0] if numberValueArray.length == 1
-      Break.each do |b|     #sum = value(Doan1)*京value + value(Doan2)*兆value+...
-        numberValueArray.each_with_index do |n, i|  
-          if (n==b) then
-            sum += kansujiHelp.segmentVal(numberValueArray.take(i))*b
-            numberValueArray = numberValueArray.drop(i+1)
-            break
-          end
+  def to_number
+    kansujiHelp = KansujiHelp.new()
+    numberValueArray = Array.new()
+    sum = 0
+    self.split('').each do |c| numberValueArray.push(Kanji[c]) end  #Convert the Kanji String to number value array
+    return numberValueArray[0] if numberValueArray.length == 1
+    Break.each do |b|     #sum = value(Doan1)*京value + value(Doan2)*兆value+...
+      numberValueArray.each_with_index do |n, i|  
+        if (n==b) then
+          sum += kansujiHelp.segmentVal(numberValueArray.take(i))*b
+          numberValueArray = numberValueArray.drop(i+1)
+          break
         end
       end
-      rest = numberValueArray.length > 0 ? numberValueArray[0]:0
-      return sum + rest #After running the loop, we can have the last part
     end
+    rest = numberValueArray.length > 0 ? numberValueArray[0]:0
+    return sum + rest #After running the loop, we can have the last part
+  end
 end
-
 class Integer
+  def to_kansuji
+    kanjiString = ""
+    number = self
+    kansujiHelp = KansujiHelp.new()
+    hasOneAtBegin = false;
+    Break.each do |b|
+      intPart = number/b
+      if intPart ==1 then
+        kanjiString += Kanji.key(b)
+        number -= b
+      end
+      if intPart > 1 then #There is value before the break
+        kanjiString += kansujiHelp.onePartToKanji(intPart) + Kanji.key(b)
+        number -= intPart*b
+      end
+    end
+    restPart = number != 0 ? Kanji.key(number): ""
+    kanjiString = "一" + kanjiString if self.digits().last == 1 and self != 1
+    return kanjiString + restPart
+  end
 end
--- kansuji/spec/kansuji_spec.rb
+++ kansuji/spec/kansuji_spec.rb
@@ -1,67 +1,85 @@
 RSpec.describe Kansuji do
   it "has a version number" do
-	expect(Kansuji::VERSION).not_to be nil
+    expect(Kansuji::VERSION).not_to be nil
   end
 
   it "should convert 三千七百十三 to number 3713" do
-	expect("三千七百十三".to_number).to eq 3713
+    expect("三千七百十三".to_number).to eq 3713
   end
 
   it "should convert 零 to 0" do
-	expect("零".to_number).to eq 0
+    expect("零".to_number).to eq 0
   end
 
   it "should convert 〇 to 0" do
-	expect("〇".to_number).to eq 0
+    expect("〇".to_number).to eq 0
   end
 
   it "should convert 一 to 1" do
-	expect("一".to_number).to eq 1
+    expect("一".to_number).to eq 1
   end
 
   it "should convert 二 to 2" do
-	expect("二".to_number).to eq 2
+    expect("二".to_number).to eq 2
   end
 
   it "should convert 三 to 3" do
-	expect("三".to_number).to eq 3
+    expect("三".to_number).to eq 3
   end
 
   it "should convert 七十五兆三千四百八十二億五千七百九十五万七千四百三十八 to 75348257957438" do
-	expect("七十五兆三千四百八十二億五千七百九十五万七千四百三十八".to_number).to eq 75348257957438
+    expect("七十五兆三千四百八十二億五千七百九十五万七千四百三十八".to_number).to eq 75348257957438
   end
 
   it "should convert 兆 to 1000000000000" do
-	expect("兆".to_number).to eq 1000000000000
+    expect("兆".to_number).to eq 1000000000000
   end
 
   it "should convert 一兆二十億三千百万四百二十一 to 1002031000421" do
-	expect("一兆二十億三千百万四百二十一".to_number).to eq 1002031000421
+    expect("一兆二十億三千百万四百二十一".to_number).to eq 1002031000421
   end
 
   it "should convert 三千百万四百二十 to 31000420" do
-	expect("三千百万四百二十".to_number).to eq 31000420
+    expect("三千百万四百二十".to_number).to eq 31000420
   end
 
   it "should convert 1 to 一" do
-	expect(1.to_kansuji).to eq "一"
+    expect(1.to_kansuji).to eq "一"
   end
 
   it "should convert 2 to 二" do
-	expect(2.to_kansuji).to eq "二"
+    expect(2.to_kansuji).to eq "二"
   end
 
   it "should convert 3 to 三" do
-	expect(3.to_kansuji).to eq "三"
+    expect(3.to_kansuji).to eq "三"
   end
 
   it "should convert 4 to 四" do
-	expect(4.to_kansuji).to eq "四"
+    expect(4.to_kansuji).to eq "四"
   end
 
   it "should convert 5 to 五" do
-	expect(5.to_kansuji).to eq "五"
+    expect(5.to_kansuji).to eq "五"
   end
 
+  it "should convert 1002031000421 to 一兆二十億三千百万四百二十一" do
+    expect(1002031000421.to_kansuji).to eq "一兆二十億三千百万四百二十一"
+  end
 
+  it "should convert 31000420 to 三千百万四百二十" do
+    expect(31000420.to_kansuji).to eq "三千百万四百二十"
+  end
+
+  it "should convert 20 to 二十" do
+    expect(20.to_kansuji).to eq "二十"
+  end
+
+  it "should convert 3100 to 三千百" do
+    expect(3100.to_kansuji).to eq "三千百"
+  end
+
+  it "should convert 75348257957438 to 七十五兆三千四百八十二億五千七百九十五万七千四百三十八" do
+    expect(75348257957438.to_kansuji).to eq "七十五兆三千四百八十二億五千七百九十五万七千四百三十八"
+  end
 end
